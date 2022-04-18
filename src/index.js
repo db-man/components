a19940946085799f9900d6657014e3637edf9e3a
Devin Chen <5204106+xxd3vin@users.noreply.github.com>
2022-04-19 00:41:31 +0800

    fix export

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 export { default as MultipleInputs } from "./components/MultipleInputs";
 export { default as SuccessMessage } from "./components/SuccessMessage";
+
 export { ImageLink } from "./components/Links";
+export { ImageLinks } from "./components/Links";
 export { Link } from "./components/Links";
 export { Links } from "./components/Links";
+export { Fragment } from "./components/Links";
 
 export { default as BreadcrumbWrapper } from "./layout/BreadcrumbWrapper";
 export { default as Settings } from "./pages/Settings";
