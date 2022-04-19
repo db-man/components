6b12535e3e170ea8fc03e3d5f07dbbda6381215b
Devin Chen <5204106+xxd3vin@users.noreply.github.com>
2022-04-19 23:05:47 +0800

    add ut

--- src/components/Links.test.jsx
+++ src/components/Links.test.jsx
@@ -1,8 +1,28 @@
 import { render, screen } from "@testing-library/react";
-import { Link } from "./Links";
+import { Link, Links } from "./Links";
 
-test("renders link", () => {
-  render(<Link>https://foo.com/</Link>);
-  const linkElement = screen.getByText(/foo/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Link", () => {
+  describe("given a text to child", () => {
+    it('renders link with "foo" text', () => {
+      render(<Link>https://foo.com/</Link>);
+      const linkElement = screen.getByText(/foo/i);
+      expect(linkElement).toBeInTheDocument();
+    });
+  });
+
+  describe("given a text to props", () => {
+    it('renders link with "bar" text', () => {
+      render(<Link href="https://foo.com/" text="bar"></Link>);
+      const linkElement = screen.getByText(/bar/i);
+      expect(linkElement).toBeInTheDocument();
+    });
+  });
+});
+
+describe("Links", () => {
+  it('renders one link with "bar" text', () => {
+    render(<Links links={[{ href: "https://foo.com/", text: "bar" }]}></Links>);
+    const linkElement = screen.getByText(/bar/i);
+    expect(linkElement).toBeInTheDocument();
+  });
 });
