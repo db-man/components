f603302678a73dd63773b84a5f27d16219627ab2
Devin Chen <5204106+xxd3vin@users.noreply.github.com>
2022-04-25 23:39:05 +0800

    exports

--- src/index.js
+++ src/index.js
@@ -1,19 +1,28 @@
-export { default as MultipleInputs } from './components/MultipleInputs';
-export { default as SuccessMessage } from './components/SuccessMessage';
-
+// components
+export { default as ListPageBody } from './components/ListPageBody';
+export { default as ErrorAlert } from './components/ErrorAlert';
 export { ImageLink } from './components/Links';
 export { ImageLinks } from './components/Links';
 export { Link } from './components/Links';
 export { Links } from './components/Links';
 export { Fragment } from './components/Links';
-export { default as ErrorAlert } from './components/ErrorAlert';
-export { default as ListPageBody } from './components/ListPageBody';
+export { default as MultipleInputs } from './components/MultipleInputs';
+export { default as RefTableLink } from './components/RefTableLink';
+export { default as RefTableLinks } from './components/RefTableLinks';
+export { default as SuccessMessage } from './components/SuccessMessage';
 
+// contexts
+export * as contexts from './contexts/page';
+
+// ddRender
+export * as ddRender from './ddRender/ddRender';
+
+// layout
 export { default as BreadcrumbWrapper } from './layout/BreadcrumbWrapper';
+
+// pages
 export { default as Settings } from './pages/Settings';
 
-export * as utils from './utils';
 export * as constants from './constants';
-export * as contexts from './contexts/page';
 export * as dbs from './dbs';
-export * as ddRender from './ddRender/ddRender';
+export * as utils from './utils';
