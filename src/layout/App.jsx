9b0e3ce7312def19187ca319ccc1144a92eb3a4e
Devin Chen <5204106+xxd3vin@users.noreply.github.com>
2022-04-30 00:06:32 +0800

    fix import css

--- src/layout/App.jsx
+++ src/layout/App.jsx
@@ -5,7 +5,7 @@ import Settings from '../pages/Settings';
 import AppLayout from './AppLayout';
 
 import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
-import './App.css';
+// import './App.css';
 
 export default function App() {
   return (
