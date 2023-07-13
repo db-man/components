2d7a92eede2d6fd53c201fa1093235847759e1fb
devin.chenyang <devin.chenyang@grabtaxi.com>
2023-07-14 00:14:59 +0800

    fix ts format

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import Example from './example';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(<Example />);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
 
 // components
 export { default as DbConnections } from './components/DbConnections';
