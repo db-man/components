2155d13783d245c8b5ed9948d018418f4167b4ed
devin.chenyang <devin.chenyang@grabtaxi.com>
2023-08-09 23:13:52 +0800

    ts

--- src/components/JsonEditor.tsx
+++ src/components/JsonEditor.tsx
@@ -1,26 +1,30 @@
-// @ts-nocheck
-
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
+import React, { useState, ChangeEvent } from 'react';
 import { Input } from 'antd';
 
-const JsonEditor = (props) => {
+interface JsonEditorProps {
+  value: Record<string, unknown>;
+  onChange: (value: Record<string, unknown>) => void;
+}
+
+const JsonEditor: React.FC<JsonEditorProps> = (props) => {
   const [jsonStr, setJsonStr] = useState(
     JSON.stringify(props.value, null, '  ')
   );
   const [errMsg, setErrMsg] = useState('');
 
-  const handleChange = (event) => {
+  const handleChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
     const { value } = event.target;
     const { onChange } = props;
 
     setErrMsg('');
     try {
-      const obj = JSON.parse(value);
+      const obj = JSON.parse(value) as Record<string, unknown>;
       onChange(obj);
     } catch (error) {
       setErrMsg(
-        `There is something wrong in JSON text, detail: ${error.message}`
+        `There is something wrong in JSON text, detail: ${
+          (error as Error).message
+        }`
       );
     }
 
@@ -33,11 +37,6 @@ const JsonEditor = (props) => {
       {errMsg && <div style={{ color: 'red' }}>{errMsg}</div>}
     </div>
   );
-};
-
-JsonEditor.propTypes = {
-  value: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
-  onChange: PropTypes.func.isRequired,
 };
 
 export default JsonEditor;
